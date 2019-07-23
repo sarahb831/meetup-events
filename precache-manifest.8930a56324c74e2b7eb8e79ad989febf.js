e1a03506a932b3404287633ee2290c6a71a306cf
SBeauchamp <smbeauchamp@mindspring.com>
2019-07-23 10:52:16 -0700

    Updates

---
+++ precache-manifest.8930a56324c74e2b7eb8e79ad989febf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2387d457f800f4dc6fc0058221a57340",
+    "url": "/meetup-events/index.html"
+  },
+  {
+    "revision": "d9edbe52c9cbf6874f07",
+    "url": "/meetup-events/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/meetup-events/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d9edbe52c9cbf6874f07",
+    "url": "/meetup-events/static/js/main.9c0e58ee.chunk.js"
+  },
+  {
+    "revision": "2654070ec8c99e337b7a",
+    "url": "/meetup-events/static/js/runtime~main.ac1a0dc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetup-events/static/media/logo.5d5d9eef.svg"
+  }
+]);
