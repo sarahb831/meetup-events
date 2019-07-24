3d0407a1a7884962fb1b3d42b2ab75b47b810fb0
SBeauchamp <smbeauchamp@mindspring.com>
2019-07-24 11:48:21 -0700

    initial commit

---
+++ auth-server/handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const axios = require('axios');
+
+module.exports.getAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+  + '?client_id=1pcniov4vgu7t6ni21bgqi5p2t'
+  + '&client_secret=6rn3iu84a6g3397hifrqv1dpsa'
+  + '&grant_type=authorization_code'
+  + '&redirect_uri=https://sarahb831.github.io/meetup-events/'
+  + '&code=' + event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
