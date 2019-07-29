c5e962ee7989b3aa6acb58a69b6c183d52935c3c
SBeauchamp <smbeauchamp@mindspring.com>
2019-07-29 13:05:04 -0700

    update CORS url

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -10,11 +10,15 @@ module.exports.getAccessToken = async (event) => {
   + '&grant_type=authorization_code'
   + '&redirect_uri=https://sarahb831.github.io/meetup-events/'
   + '&code=' + event.pathParameters.code;
+ // + '&code=fe279b2c4b51c2681ffe1f69d4ebee06';
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       access_token: info.data.access_token,
       refresh_token: info.data.refresh_token,
