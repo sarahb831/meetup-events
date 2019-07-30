81add8793de0936464aa262e2376216e5d082c91
SBeauchamp <smbeauchamp@mindspring.com>
2019-07-30 08:15:39 -0700

    add refresh function and cors updates

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -10,14 +10,37 @@ module.exports.getAccessToken = async (event) => {
   + '&grant_type=authorization_code'
   + '&redirect_uri=https://sarahb831.github.io/meetup-events/'
   + '&code=' + event.pathParameters.code;
- // + '&code=fe279b2c4b51c2681ffe1f69d4ebee06';
+ // + '&code=1d81994f83b2cda10f78ddf000bec98f';
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
+
+module.exports.getRefreshToken =async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+  + '?client_id=1pcniov4vgu7t6ni21bgqi5p2t'
+  + '&client_secret=6rn3iu84a6g3397hifrqv1dpsa'
+  + '&grant_type=refresh_token'
+  + '&refresh_token=' + event.pathParameters.code;
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify({
       access_token: info.data.access_token,
