7ecf526fb3c15de1271b499514bfec1bc9c30a0b
SBeauchamp <smbeauchamp@mindspring.com>
2019-08-09 09:56:50 -0700

    add new files for acceptance tsts

---
+++ src/features/filterEventsByCity.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { mount, shallow } from  'enzyme';
+import App from  '../App';
+import CitySearch from '../CitySearch';
+import { loadFeature, defineFeature } from 'jest-cucumber';
+import { mockEventsSingle } from  '../mock-events';
+
+const feature = loadFeature('./src/features/filterEventsByCity.feature');
+defineFeature(feature, test => {
+    test('By default, when user hasn’t searched for a city, show upcoming events based on the user’s location', ({ given, when, then }) => {
+    	given('user hasn\'t searched for any city', () => {
+
+    	});
+        let AppWrapper;
+    	when('the user opens the app', () => {
+            AppWrapper = mount(< App />);
+    	});
+
+    	then('the user should see the list of upcoming events from their location', () => {
+            AppWrapper.update();
+            expect(AppWrapper.find('.Event')).toHaveLength(mockEventsSingle.events.length);
+    	});
+    });
+
+    test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+        let CitySearchWrapper;
+        given('the main page is open', () => {
+            CitySearchWrapper = shallow(< CitySearch />);
+    	});
+
+    	when('user starts typing in the city textbox', () => {
+            CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Munich' }});
+    	});
+
+    	then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
+
+    	});
+    });
+
+    test('User can select a city from the suggested list', ({ given, and, when, then }) => {
+    	given('user was typing “Munich” in the city textbox', () => {
+
+    	});
+
+    	and('the list of suggested cities is showing', () => {
+
+    	});
+
+    	when('the user selects a city (e.g., “Munich, Germany”) from the list', () => {
+
+    	});
+
+    	then('their city should be changed to that city (i.e., “Munich, Germany”)', () => {
+
+    	});
+
+    	and('the user should receive a list of upcoming events in that city', () => {
+
+    	});
+    });
+
+});
