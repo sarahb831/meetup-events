d01a5ce61dabc7f2d96e4bddbff39cdbe80b64ee
SBeauchamp <smbeauchamp@mindspring.com>
2019-08-16 08:56:43 -0700

    add support for localStorage of events

--- src/api.js
+++ src/api.js
@@ -47,6 +47,11 @@ async function getEvents(lat=null, lon=null, numberToDisplay) {
         return partialEventsList;
     }
 
+    if (!navigator.onLine) {
+        const events = localStorage.getItem('lastEvents');
+        return JSON.parse(events);
+    }
+    
     const token = await getAccessToken();
     if (token) {
         let url = 'https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public'
@@ -62,7 +67,11 @@ async function getEvents(lat=null, lon=null, numberToDisplay) {
             url += '&lat=' + lat;
         }
         const result = await axios.get(url);
-        return result.data.events;
+        const events = result.data.events;
+        if (events.length) {  //make sure events exist
+            localStorage.setItem('lastEvents', JSON.stringify(events));
+        }
+        return events;
     }
     return [];
 }
