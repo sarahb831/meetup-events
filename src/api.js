6cfd04cf57902f75ab3c313ef3adc60eacb60745
SBeauchamp <smbeauchamp@mindspring.com>
2019-08-16 08:04:30 -0700

    add workaround for expired accessToken

--- src/api.js
+++ src/api.js
@@ -82,11 +82,17 @@ async function getOrRenewAccessToken(type, key) {
 
     // use Axios to make GET request to endpoint
     const tokenInfo = await axios.get(url);
-  //  if (tokenInfo.statusCode !== 200 && type ==='renew') {
-        // redirect to Meetup API to get new auth code
-   //     window.location.href='https://secure.meetup.com/oauth2/authorize?client_id=1pcniov4vgu7t6ni21bgqi5p2t&response_type=code&redirect_uri=https://sarahb831.github.io/meetup-events/';
-    //    return null;
-  //  }
+    // check for valid response
+    if (!tokenInfo || (tokenInfo.statusCode !== 200 && type ==='renew')) {
+        //redirect to Meetup API to get new auth code
+        console.log("invalid token");
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        localStorage.removeItem('last_saved_time');
+    
+        window.location.href='https://secure.meetup.com/oauth2/authorize?client_id=1pcniov4vgu7t6ni21bgqi5p2t&response_type=code&redirect_uri=https://sarahb831.github.io/meetup-events/';
+        return null;
+    }
 
     // save tokens to local storage with timestamp
     localStorage.setItem('access_token', tokenInfo.data.access_token);
@@ -100,6 +106,15 @@ async function getOrRenewAccessToken(type, key) {
 
 function getAccessToken() {
     const accessToken = localStorage.getItem('access_token');
+    const lastSavedTime = localStorage.getItem('last_saved_time') ? localStorage.getItem('last_saved_time') : Date.now();
+    const expiredToken = Date.now() - lastSavedTime >= 3600000;
+
+    if (expiredToken) {
+        console.log("expired token");
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        localStorage.removeItem('last_saved_time');
+    }
 
     if (!accessToken) {
         const searchParams = new URLSearchParams(window.location.search); // check for auth code in url
@@ -112,14 +127,14 @@ function getAccessToken() {
         return getOrRenewAccessToken('get', code);
     }
 
-    const lastSavedTime = localStorage.getItem('last_saved_time');
+    //const lastSavedTime = localStorage.getItem('last_saved_time');
     if (accessToken && (Date.now() - lastSavedTime < 3600000)) {
         return accessToken;
     }
 
     // if access_token is expired try to renew it using refresh_token
-    const refreshToken = localStorage.getItem('refresh_token');
-    return getOrRenewAccessToken('renew', refreshToken);
+   // const refreshToken = localStorage.getItem('refresh_token');
+   // return getOrRenewAccessToken('renew', refreshToken);
 };
 
 export { getSuggestions, getEvents };
